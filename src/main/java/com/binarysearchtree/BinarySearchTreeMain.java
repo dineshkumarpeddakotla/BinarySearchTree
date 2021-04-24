031d93a31773247948590e84d39dd97fb3a9a6e6
dinnu247 <dineshkumar.icon@gmail.com>
2021-04-24 20:52:56 +0530

    CREATED: Binary Search Tree With Welcome Message

---
+++ src/main/java/com/binarysearchtree/BinarySearchTreeMain.java
@@ -0,0 +1,7 @@
+package com.binarysearchtree;
+
+public class BinarySearchTreeMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Binary Search Tree");
+    }
+}
